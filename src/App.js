1513f26f8bff2e8f8248c17a0ebd587b59b7d184
samuel426 <samuel2626@khu.ac.kr>
2024-02-23 17:10:42 +0900

    5강 최적화 2 React memo 마무리 버전

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState, useMemo } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import OptimizeTest from "./OptimizeTest";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const dummyList = [
+//   {
+//     id:1,
+//     author: "우성현",
+//     content: "안녕 1",
+//     emotion:5,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id:2,
+//     author: "우",
+//     content: "안녕 2",
+//     emotion:3,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id:3,
+//     author: "성",
+//     content: "안녕 3",
+//     emotion:1,
+//     created_date: new Date().getTime()
+//   }
+// ];
+
+// https://jsonplaceholder.typicode.com/comments
+
+const App = () => {
+
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const getData = async()=>{
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+    ).then((res)=>res.json());
+    console.log(res);
+
+    const initData = res.slice(0,20).map((it)=>{
+      return {
+        author : it.email,
+        content : it.body,
+        emotion : Math.floor(Math.random() * 5) +1,
+        created_date : new Date().getTime(),
+        id : dataId.current++
+      }
+    });
+    setData(initData);
+    
+  }
+  useEffect(()=>{
+    getData();
+  },[])
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) =>{
+    setData(
+      data.map((it)=>it.id === targetId ? {...it, content:newContent} : it)
+    )
+  };
+
+  const getDiaryAnalysis = useMemo(() => {
+
+    const goodCount = data.filter((it)=>it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return {goodCount, badCount, goodRatio};
+  }, [data.length]);
+
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
+
+    return (
+      <div className="App">
+        <OptimizeTest/>
+        <DiaryEditor onCreate={onCreate}/>
+        <div>전체일기 : {data.length}</div>
+        <div>기분 좋은 일기 개수 : {goodCount}</div>
+        <div>기분 나쁜 일기 개수 : {badCount}</div>
+        <div>기분 좋은 일기 비율 : {goodRatio}</div>
+        <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/> 
+      </div>
+    );
 }
 
 export default App;
