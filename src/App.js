2e97cc856fc256b65df2f8b028e4d011ea16241b
samuel426 <samuel2626@khu.ac.kr>
2024-02-27 16:38:16 +0900

    한입리액트 5강 context를 이용해 컴포넌트 최적화하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useMemo, useCallback, useReducer } from "react";
+import React, { useEffect, useRef, useMemo, useCallback, useReducer } from "react";
+
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -56,10 +57,13 @@ const reducer = (state, action)=> {
   }
 };
 
+export const DiaryStateContext = React.createContext();
+
+export const DiaryDispatchContext = React.createContext();
+
 
 const App = () => {
 
-  //const [data, setData] = useState([]);
 
   const [data, dispatch] = useReducer(reducer, []);
 
@@ -105,6 +109,10 @@ const App = () => {
     dispatch({type:"EDIT", targetId, newContent});
   }, []);
 
+  const memoizedDispatches = useMemo(()=>{
+    return {onCreate, onRemove, onEdit}
+  },[])
+
   const getDiaryAnalysis = useMemo(() => {
 
     const goodCount = data.filter((it)=>it.emotion >= 3).length;
@@ -116,14 +124,18 @@ const App = () => {
   const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
 
     return (
-      <div className="App">
-        <DiaryEditor onCreate={onCreate}/>
-        <div>전체일기 : {data.length}</div>
-        <div>기분 좋은 일기 개수 : {goodCount}</div>
-        <div>기분 나쁜 일기 개수 : {badCount}</div>
-        <div>기분 좋은 일기 비율 : {goodRatio}</div>
-        <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/> 
-      </div>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={memoizedDispatches}>
+          <div className="App">
+            <DiaryEditor/>
+            <div>전체일기 : {data.length}</div>
+            <div>기분 좋은 일기 개수 : {goodCount}</div>
+            <div>기분 나쁜 일기 개수 : {badCount}</div>
+            <div>기분 좋은 일기 비율 : {goodRatio}</div>
+            <DiaryList/> 
+          </div>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     );
 }
 
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,17 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
+import { DiaryDispatchContext } from "./App";
 
 const DiaryItem = ({
-    onEdit,
-    onRemove,
     author, 
     content, 
     created_date, 
     emotion, 
     id
 }) => {
-    useEffect(()=> {
-        console.log(`${id}번 째 아이템 랜더!`);
-    });
+
+    const {onEdit, onRemove} = useContext(DiaryDispatchContext);
 
     const [isEdit, setIsEdit] = useState(false);
     const toggleIsEdit = () =>  setIsEdit(!isEdit);
